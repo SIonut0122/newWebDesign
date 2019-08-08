07b15d8679ac09683054423b4e6e4f6ab0d6f7c5
Sionut0122 <sionut0122@yahoo.com>
2019-08-08 22:39:29 +0100

    Updates

---
+++ precache-manifest.375a72558c7be943b99de2f59490ea26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287ba26b73d1781a3987370048b9bb37",
+    "url": "/newWebDesign/index.html"
+  },
+  {
+    "revision": "8ad353689599097f5fad",
+    "url": "/newWebDesign/static/css/main.e5535c6c.chunk.css"
+  },
+  {
+    "revision": "2719d7720ce5692978ef",
+    "url": "/newWebDesign/static/js/2.08a4ba02.chunk.js"
+  },
+  {
+    "revision": "8ad353689599097f5fad",
+    "url": "/newWebDesign/static/js/main.3ff44a3e.chunk.js"
+  },
+  {
+    "revision": "18482b6c52e4a3475ea8",
+    "url": "/newWebDesign/static/js/runtime~main.c3741077.js"
+  },
+  {
+    "revision": "57dfdf954c8776b2d4680a78a2824bc6",
+    "url": "/newWebDesign/static/media/Light_Leak_Motion.57dfdf95.mp4"
+  },
+  {
+    "revision": "6a3de149e6a84114fe040b7bfd4bfe9a",
+    "url": "/newWebDesign/static/media/joshua-earle.6a3de149.jpg"
+  },
+  {
+    "revision": "367663998567d60633500273a285baac",
+    "url": "/newWebDesign/static/media/logo.36766399.png"
+  },
+  {
+    "revision": "9de3e9bf604f7fdd9c3032e194c53993",
+    "url": "/newWebDesign/static/media/matthew-henry.9de3e9bf.jpg"
+  },
+  {
+    "revision": "e5e263eb1f9ac3f1454ff900a21c17af",
+    "url": "/newWebDesign/static/media/noah.e5e263eb.jpg"
+  },
+  {
+    "revision": "e6e108997bd881eba91294e0061a9752",
+    "url": "/newWebDesign/static/media/stars.e6e10899.png"
+  },
+  {
+    "revision": "0eb88afbe555aefe02aa20c37d9cb165",
+    "url": "/newWebDesign/static/media/super_88.0eb88afb.mov"
+  },
+  {
+    "revision": "3408054e5f083993c59b1b92204fbc12",
+    "url": "/newWebDesign/static/media/trans.3408054e.png"
+  }
+]);
